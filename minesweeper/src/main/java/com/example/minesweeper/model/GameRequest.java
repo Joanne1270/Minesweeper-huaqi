e4dd2a8a26ae53c83f7c95e4d729710538cbd276
Joanne1270 <24115037d@connect.polyu.hk>
2026-05-21 21:10:22 +0800

    Update GameRequest.java

--- minesweeper/src/main/java/com/example/minesweeper/model/GameRequest.java
+++ minesweeper/src/main/java/com/example/minesweeper/model/GameRequest.java
@@ -2,6 +2,9 @@ package com.example.minesweeper.model;
 
 public class GameRequest {
     private String difficulty;
+    private int rows;
+    private int cols;
+    private int mines;
 
     public String getDifficulty() {
         return difficulty;
@@ -9,5 +12,29 @@ public class GameRequest {
 
     public void setDifficulty(String difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+    public int getMines() {
+        return mines;
+    }
+
+    public void setMines(int mines) {
+        this.mines = mines;
     }
 }
